87739ff68b89a54302eec6386f7a367e41680aa1
swetha123 <swetha.havalkod@gmail.com>
2024-02-03 17:48:26 +0530

    model training completed

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -40,6 +40,11 @@ class DataIngestion:
 
             logging.info("data ingestion part is completed")
 
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
         except Exception as e:
             logging.info("exception occured at the data ingestion phase ")
             raise CustomException(e,sys)    
